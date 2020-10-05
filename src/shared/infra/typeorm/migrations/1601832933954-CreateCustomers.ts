ffdf5934a37bb4ad16de84dfb0c1a55e17fada94
zagob <matheusus1_23@hotmail.com>
2020-10-04 22:17:33 -0300

    correção arquivo CreateCustomers

--- src/shared/infra/typeorm/migrations/1601832933954-CreateCustomers.ts
+++ src/shared/infra/typeorm/migrations/1601832933954-CreateCustomers.ts
@@ -12,6 +12,7 @@ export default class CreateCustomers1601832933954 implements MigrationInterface 
               type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
+              default: 'uuid_generate_v4()',
             },
             {
               name: 'name',
